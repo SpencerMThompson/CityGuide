b58869c4ca2bb2719712a74f3d850748187a5739
Spencer Thompson <st746866@southeast.edu>
2023-02-09 13:41:56 -0500

    commit

--- App.js
+++ App.js
@@ -1,20 +1,122 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+ // ListView,
+  FlatList,
+  Image,
+  Text,
+} from 'react-native';
+//import ListView from 'deprecated-react-native-listview';
+import data from './attractions.json';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   const dataSource = new ListView.DataSource({
+  //     rowHasChanged: (r1, r2) => r1 !== r2
+  //   });
+
+  //   this.state = {
+  //     dataSource: dataSource.cloneWithRows(data),
+  //   };
+  // }
+
+  renderRow({item}) {
+    return (
+      <View style={styles.row}>
+        <View style={styles.iconContainer}>
+          <Image source={basketIcon} style={styles.icon} />
+        </View>
+        <View style={styles.info}>
+          <Text style={styles.items}>{item.items} Items</Text>
+          <Text style={styles.address}>{item.address}</Text>
+        </View>
+        <View style={styles.total}>
+          <Text style={styles.date}>{item.date}</Text>
+          <Text style={styles.price}>${item.total}</Text>
+        </View>
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.mainContainer}>
+        <Text style={styles.title}>Sales</Text>
+        {/* <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow}
+        /> */}
+        <FlatList  data={data} renderItem={this.renderRow}></FlatList>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  title: {
+    backgroundColor: '#0f1b29',
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+    padding: 10,
+    paddingTop: 40,
+    textAlign: 'center',
+  },
+  row: {
+    borderColor: '#f1f1f1',
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+    marginLeft: 10,
+    marginRight: 10,
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+  iconContainer: {
+    alignItems: 'center',
+    backgroundColor: '#feb401',
+    borderColor: '#feaf12',
+    borderRadius: 25,
+    borderWidth: 1,
+    justifyContent: 'center',
+    height: 50,
+    width: 50,
+  },
+  icon: {
+    tintColor: '#fff',
+    height: 22,
+    width: 22,
+  },
+  info: {
+    flex: 1,
+    paddingLeft: 25,
+    paddingRight: 25,
+  },
+  items: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  address: {
+    color: '#ccc',
+    fontSize: 14,
+  },
+  total: {
+    width: 80,
+  },
+  date: {
+    fontSize: 12,
+    marginBottom: 5,
+  },
+  price: {
+    color: '#1cad61',
+    fontSize: 25,
+    fontWeight: 'bold',
+  }
 });
